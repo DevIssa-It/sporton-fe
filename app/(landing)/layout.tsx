ce60fd9b9328ccc3a4dc0c2dc3bfbe8ecd78cfa4
Qiforiss67 <issaitama07@gmail.com>
2026-01-07 19:16:31 +0700

    Refactor code structure for improved readability and maintainability

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Headers from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
 import "../globals.css";
 
 const poppins = Poppins({
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <Headers />
         {children}
+        <Footer />
       </body>
     </html>
   );
